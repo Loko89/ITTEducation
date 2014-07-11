44a2e2e5800eb0eb2e7dabc6712776882369c5f5
tpetrov <tpetrov@tpetrov-PC>
2014-07-11 15:44:47 +0300

    Class that reverses an array

---
+++ IntroducingJava/src/com/sirma/itt/javacourse/intro/run/ArrayReverse.java
@@ -0,0 +1,36 @@
+package com.sirma.itt.javacourse.intro.run;
+import java.util.Scanner;
+
+import com.sirma.itt.javacourse.utility.MathUtility;
+/**
+ * Program for finding an array's medyan.
+ * @author tpetrov
+ *
+ */
+public final class ArrayReverse {
+	/**
+	 * Constructor.
+	 */
+	private ArrayReverse(){
+		
+	}
+	/**
+	 * 
+	 * @param args global param.
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner input = new Scanner(System.in);
+		int[]myArray = new int[15];
+		for(int iterator = 0; iterator < myArray.length; iterator++ ){
+			System.out.printf("Input myArray["+iterator+"]: ");
+			myArray[iterator] = input.nextInt();
+		}
+		myArray = MathUtility.reverse(myArray);
+		for (int i =0; i<myArray.length;i++ ){
+			System.out.printf(myArray[i]+",");
+		}
+		input.close();
+	}
+
+}
